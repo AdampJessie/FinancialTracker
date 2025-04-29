0f6f630dc95bd9559762fb778d266fdf976c5c1b
unknown <adampjessie@gmail.com>
2025-04-29 15:09:34 -0400

    Refactored code, added conditional outputs

--- src/main/java/com/pluralsight/FinancialTracker.java
+++ src/main/java/com/pluralsight/FinancialTracker.java
@@ -157,32 +157,41 @@ public class FinancialTracker {
 
     private static void displayDeposits() {
 
-        System.out.println("+" + "-".repeat(90) + "+");
-        System.out.printf("| %-10s | %-8s | %-30s | %-20s | %-8s |\n",
-                "Date", "Time", "Description", "Vendor", "Amount");
-        System.out.println("+" + "-".repeat(90) + "+");
-        for (Transaction i : transactions) {
-            if (i.getAmount() > 0) {
-                System.out.println(i);
-                System.out.println("+" + "-".repeat(90) + "+");
+        boolean found = false;
+        for (Transaction i : transactions)
+            if (i.getAmount() < 0) {
+                found = true;
+                break;
             }
-        }
+        if (found) {
+            printColumn(true);
+            for (Transaction i : transactions) {
+                if (i.getAmount() < 0) {
+                    System.out.println(i);
+                    System.out.println("+" + "-".repeat(90) + "+");
+                }
+            }
+        }else printColumn(false);
     }
 
     private static void displayPayments() {
         // This method should display a table of all payments in the `transactions` ArrayList.
         // The table should have columns for date, time, description, vendor, and amount.
-
-        System.out.println("+" + "-".repeat(90) + "+");
-        System.out.printf("| %-10s | %-8s | %-30s | %-20s | %-8s |\n",
-                "Date", "Time", "Description", "Vendor", "Amount");
-        System.out.println("+" + "-".repeat(90) + "+");
-        for (Transaction i : transactions) {
+        boolean found = false;
+        for (Transaction i : transactions)
             if (i.getAmount() < 0) {
-                System.out.println(i);
-                System.out.println("+" + "-".repeat(90) + "+");
+                found = true;
+                break;
             }
-        }
+        if (found) {
+            printColumn(true);
+            for (Transaction i : transactions) {
+                if (i.getAmount() < 0) {
+                    System.out.println(i);
+                    System.out.println("+" + "-".repeat(90) + "+");
+                }
+            }
+        }else printColumn(false);
     }
 
 
